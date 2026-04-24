137012f55cd89da0870c78a222e1f62fd0357e16
Sakshi Davra <sakshidavra5@gmail.com>
2026-04-24 11:58:19 +0000

    dynamic option in builder

---
+++ pos_changes/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'POS Change',
+    'version': '1.0',
+    'depends': ['point_of_sale'],
+    'data': [],
+    'assets': {},
+    'installable': True,
+
+
+
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_changes/static/src/js/custom_button.js',
+            'pos_changes/static/src/xml/custom_button.xml',
+        ],
+    },
+}
