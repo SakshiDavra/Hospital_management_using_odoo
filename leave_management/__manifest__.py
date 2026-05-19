7f93d8853014b892c77badc2c941418ae00accad
Sakshi Davra <sakshidavra5@gmail.com>
2026-05-19 04:32:34 +0000

    hr module time off

---
+++ leave_management/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Leave Management',
+    'version': '1.0',
+    'depends': ['base', 'hr', 'hr_holidays'],
+
+    'data': [
+        'views/hr_leave_views.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+}
---
+++ leave_management/models/leave_request.py
@@ -0,0 +1,16 @@
+from odoo import api, models
+
+class HrLeave(models.Model):
+    _inherit = "hr.leave"
+
+    @api.onchange(
+        'request_date_from_period',
+        'request_date_to_period'
+    )
+    def _onchange_half_day_custom(self):
+
+        self.request_unit_half = True
+
+        self._compute_date_from_to()
+
+        self._compute_duration()
